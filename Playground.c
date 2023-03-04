b337f86f6b997d27b664533bbe7f374a9a0b79c3
Ahmad <ahmadmtr@protonmail.com>
2023-03-04 07:30:36 +0300

    v0.01

--- Playground.c
+++ Playground.c
@@ -7,32 +7,67 @@ struct Exam {
     char Name[64];
     char date_st[5];
     char date_end[6];
-    int date;
-    int month;
-    int year;
+    char date[2];
+    char month[2];
+    char year[4];
     char day[3];
     char rooms[64];
 };
 
-char *strtok(char *str, const char *delimiters);
+//char *strtok(char *str, const char *delimiters);
 
 int main() {
     struct Exam infoarr[2];
-    int A = 1, B = 2, C = 3, D = 4, E = 5, F = 6, G = 7, H = 8, I = 9;
+    int A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7, I = 8;
     char str[] = "CS415\tSystems Programming\t08:30-10:30\t21-01-2023 , Sat\tM424+M426\n"
                  "ACC598B\tSpecial Topics in Accounting\t08:30-10:30\t21-01-2023 , Sat\tB018";
     char *pch;
-    printf("Splitting string \"%s\" into tokens:\n", str);
+    printf("Splitting string \"\" into tokens:\n");
     pch = strtok(str, "\t\n,.-");
-    for (int token_counter = 0; pch != NULL; token_counter++) {
-        printf("%s\t%d\n", pch, token_counter);
-        for (int i = 0; i < 10; ++i) {
-            if (token_counter == A-1){
-
+    int token_counter = 0, info_ap_index=-9;
+    for (int s = 0; s < 2; ++s) {
+        while (pch != NULL){
+            printf("%s\t%d\n",pch, token_counter);
+            for (info_ap_index+=9; info_ap_index < (info_ap_index+9); ++info_ap_index) {
+                if (token_counter == A){
+                    strcpy(infoarr[s].code, pch);
+                    A+=9;
+                } else if(token_counter == B){
+                    strcpy(infoarr[s].Name, pch);
+                    B+=9;
+                } else if(token_counter == C){
+                    strcpy(infoarr[s].date_st, pch);
+                    C+=9;
+                }
+                else if(token_counter == D){
+                    strcpy(infoarr[s].date_end, pch);
+                    D+=9;
+                }
+                else if(token_counter == E){
+                    strcpy(infoarr[s].date, pch);
+                    E+=9;
+                }
+                else if(token_counter == F){
+                    strcpy(infoarr[s].month, pch);
+                    F+=9;
+                }
+                else if(token_counter == G){
+                    strcpy(infoarr[s].year, pch);
+                    G+=9;
+                }
+                else if(token_counter == H){
+                    strcpy(infoarr[s].day, pch);
+                    H+=9;
+                } else{
+                    strcpy(infoarr[s].rooms,pch);
+                    I+=9;
+                }
             }
+            token_counter++;
+
+            pch = strtok(NULL, "\t\n,.-");
+
         }
-        //token_counter++;
-        pch = strtok(NULL, "\t\n,.-");
     }
     for (int i = 0; i < 2; ++i) {
         printf("Exam #%d\ncode:%s  name:%s  %s-%s   %d/%d/%d %s   %s\n", i + 1, infoarr[i].code, infoarr[i].Name,
